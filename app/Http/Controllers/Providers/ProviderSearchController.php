4375824ca4949863349fa7483d14f8b3f9f9c92e
fotima <fkhayrullayeva97@gmail.com>
2024-09-06 16:24:15 +0200

    fixed error

--- app/Http/Controllers/Providers/ProviderSearchController.php
+++ app/Http/Controllers/Providers/ProviderSearchController.php
@@ -10,7 +10,7 @@ class ProviderSearchController extends Controller
     public function search(Request $request)
     {
         $query = $request->input('query');
-        dd($query)
+        dd($query);
         $providers = Provider::where('name', 'LIKE', "%{$query}%")->take(5)->get();
 
         return response()->json($providers);
